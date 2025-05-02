6e4b011c5ac440ded78532e26d47b2f1ed3b2e6c
Esmail Sameh <esmil.sameh2005@gmail.com>
2025-05-02 23:23:32 +0300

    add data to mongo

--- app.js
+++ app.js
@@ -2,22 +2,35 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
+const userData = require("./model/myDataSchema");
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile("./view/index.html", { root: __dirname });
 });
 
-
-
 mongoose
   .connect(
-    "mongodb+srv://esmil_sameh:12a3HmyBKur48NKs@cluster0.ibg8pjp.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://esmil_sameh:12a3HmyBKur48NKs@cluster0.ibg8pjp.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => {
     app.listen(port, () => {
-        console.log(`http://localhost:${port}`);
-      });
+      console.log(`http://localhost:${port}`);
+    });
   })
   .catch((error) => {
     console.log(error);
   });
+
+app.post("/", (req, res) => {
+  const data = new userData(req.body);
+  console.log(req.body);
+
+  data.save().then(
+    res.redirect("./view/index.html")
+  ).catch(
+    (error)=>{
+      console.log(error);
+    }
+  );
+});
