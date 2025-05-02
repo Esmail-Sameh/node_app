ab33a6ebc6f1e159655bf04d5c486a87ac78c6f9
Esmail Sameh <esmil.sameh2005@gmail.com>
2025-05-02 06:05:43 +0300

    connection with mongo

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
-    res.sendFile("./view/index.html", { root: __dirname });
+  res.sendFile("./view/index.html", { root: __dirname });
 });
 
-app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
-});
+
+
+mongoose
+  .connect(
+    "mongodb+srv://esmil_sameh:12a3HmyBKur48NKs@cluster0.ibg8pjp.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0",
+  )
+  .then(() => {
+    app.listen(port, () => {
+        console.log(`http://localhost:${port}`);
+      });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
